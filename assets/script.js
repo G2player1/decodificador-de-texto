203d654a2a77e5d10ceea46bc77edfe80679a1d4
Enos Henrique <enoshenrique008@gmail.com>
2024-08-22 01:58:59 -0300

    começo da criação das funções js

---
+++ assets/script.js
@@ -0,0 +1,13 @@
+function criptografar(){
+    document.getElementById('img__exibe-texto').style.display = "none";
+    document.getElementById('no-msg__exibe-texto').style.display = "none";
+    document.getElementById('texto__exibe-texto').style.display = "block";
+    document.getElementById('copiar').style.display = "block";
+}
+
+function discriptografar(){
+    document.getElementById('img__exibe-texto').style.display = "none";
+    document.getElementById('no-msg__exibe-texto').style.display = "none";
+    document.getElementById('texto__exibe-texto').style.display = "block";
+    document.getElementById('copiar').style.display = "block";
+}
